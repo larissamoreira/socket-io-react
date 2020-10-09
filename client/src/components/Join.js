b8cfe58b83ba385e44ad0f46064015880e2a33e0
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-09 14:04:04 -0300

    Join Component

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Join = () => {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+
     return (
-        <h1>Join</h1>
+        <>
+            <input
+                type="text"
+                onChange={e => setName(e.target.value)}
+            />
+            <input
+                type="text"
+                onChange={e => setRoom(e.target.value)}
+            />
+            <Link 
+                onClick={event => (!name || !room) ? event.preventDefault() : null}
+                to={`/chat?name=${name}&room=${room}`}>
+                <button type="submit">Sign in</button>
+            </Link>
+        </>
     )
 }
 
