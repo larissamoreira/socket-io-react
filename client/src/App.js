d301f0724c16f3cc858d2c5916acfb77e4b117f3
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-09 12:55:33 -0300

    server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function App() {
-    return (
-        <div>
-            Hello!
-        </div>
-    )
-}
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Join from './components/Join'
+import Chat from './components/Chat'
+
+const App = () => (
+    <Router>
+        <Route path="/" exact component={Join}/>
+        <Route path="/chat" component={Chat}/>
+    </Router>
+)
 
 export default App
